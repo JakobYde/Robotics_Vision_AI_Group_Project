b885031b7053ea20fe23411d9abb7784627dd6ab
simonlbs <simonlyckbjaert@hotmail.com>
2018-10-11 13:32:13 +0200

    Better installer

--- install.sh
+++ install.sh
@@ -3,23 +3,23 @@
 sudo apt-get update
 sudo apt-get upgrade
 echo Git install
-sudo apt install git
+sudo apt install -y git
 echo Gazebo dev  install
-sudo apt install libgazebo9-dev
+sudo apt install -y libgazebo9-dev
 echo OPencv install
-sudo apt install libopencv-dev
+sudo apt install -y libopencv-dev
 echo QT5 qmake install
-sudo apt install qt5-qmake
+sudo apt install -y qt5-qmake
 echo QTcreator install
-sudo apt install qtcreator
+sudo apt install -y qtcreator
 echo Gazebo install
-sudo apt install gazebo9
+sudo apt install -y gazebo9
 echo OPenctm install
-sudo apt intall openctm-tools
+sudo apt intall -y openctm-tools
 echo Pstoedit install
-sudo apt install pstoedit
+sudo apt install -y pstoedit
 echo Postrace install
-sudo apt install potrace
+sudo apt install -y potrace
 echo OPenSCAD install
 sudo add-apt-repository ppa:openscad/releases
-sudo apt-get install openscad
+sudo apt-get install -y openscad
